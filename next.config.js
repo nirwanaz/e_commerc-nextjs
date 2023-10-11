780ab901b2976158dfc1861562dd43424494a01f
nirwanaz <90511965+nirwanaz@users.noreply.github.com>
2023-10-11 09:09:35 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const nextConfig = {
         DB_URI: "mongodb://localhost:27017/buyitnow",
 
         CLOUD_NAME: "dl2uzcyhl",
-        CLOUDINARY_API_KEY: "999747855934663",
-        CLOUDINARY_API_SECRET: "x8O_C8eYNvYzMgQd8rJXn6HlVLc",
+        CLOUDINARY_API_KEY: "",
+        CLOUDINARY_API_SECRET: "",
 
-        STRIPE_PRIVATE_KEY: "sk_test_51Nwx1WGun9cYQToJcDIGX1AWAUbc51Rah6gj6wQxcCU9UrVUz9dprJWot5QLGd8C3YdG7t0hjzGZHmTKIV6zVXBA009uoJ2w05",
-        STRIPE_WEBHOOK_SECRET: "whsec_fc9e2910950a05e4dddd416cdaf629fb1346f5b0ef5965f3a097bed7cd15e94f",
+        STRIPE_PRIVATE_KEY: "",
+        STRIPE_WEBHOOK_SECRET: "",
 
         NEXTAUTH_SECRET: "codingwithzeroxone",
     },
